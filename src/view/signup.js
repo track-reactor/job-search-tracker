f173b5fd8365d7c583705c865e90847bc3436dd2
Andrew Lee <andrewlee@fb.com>
2018-03-23 22:45:56 -0700

    Add signup input and error handling

--- src/view/signup.js
+++ src/view/signup.js
@@ -1,5 +1,3 @@
-/* @flow */
-
 import React from 'react';
 
 //css
@@ -8,9 +6,73 @@ require('../css/signup.css');
 type Props = {||};
 
 class Signup extends React.Component<Props> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      userName: '',
+      password: '',
+      confirmPassword: '',
+      confirmPasswordError: false,
+    }
+  }
+
+  _onInputChange(value, key) {
+    let stateObj = { [key]:value };
+
+    if (key === 'confirmPassword') {
+      let password = this.state.password
+      if (password !== value) {
+        stateObj.confirmPasswordError = true;
+      } else {
+        stateObj.confirmPasswordError = false;
+      }
+    }
+    this.setState(stateObj)
+  }
+
 	render() {
+    const { userName, password, confirmPassword, confirmPasswordError } = this.state;
+    console.log(this.state)
 		return (
-			<div>Signup</div>
+      <div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+            id="userName"
+            value={userName}
+            onChange={(e) => {this._onInputChange.call(this, e.target.value, 'userName')}}
+            type="text"
+            className="validate">
+          </input>
+          <label for="userName">User Name</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+            id="password"
+            value={password}
+            onChange={(e) => {this._onInputChange.call(this, e.target.value, 'password')}}
+            type="password"
+            className="validate">
+          </input>
+          <label for="userName">Password</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => {this._onInputChange.call(this, e.target.value, 'confirmPassword')}}
+            type="password"
+            className={confirmPasswordError ? 'validate error' : 'validate'}>
+          </input>
+          <label for="userName">Confirm Password</label>
+        </div>
+      </div>
+      </div>
 		)
 	}
 }
